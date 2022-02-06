0bb3fdb95510070c4ebb426f661ef957e36610eb
k <patriotarrow2@gmail.com>
2022-02-06 10:07:52 +0900

    fastcommit

---
+++ pages/components/Login.js
@@ -0,0 +1,17 @@
+
+
+const Login = () => {
+  return (
+    <div className="h-screen bg-gray-100 flex flex-col justify-center items-center">
+     <div className="bg-white mb-3 border border-gray-300 w-80 pt-8 pb-4 flex flex-col items-center ">
+      <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" className="h-12 w-44" alt="" />
+      <div>Something</div>
+         </div>
+         <div className="bg-white">
+         another dic
+        </div>
+    </div>
+  )
+}
+
+export default Login
